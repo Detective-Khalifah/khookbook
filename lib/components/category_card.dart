e3454ac044610c4c40a2ad2fc82744f42eeadb7f
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-07-14 01:45:32 +0100

    Remove padding from the category_card.dart Replace CircleAvatar with ClipRRect to display thumbnail of {@link CategoryCard}s Set random {@link Color} from pre-defined lists for Categories list & Meals List

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CategoryCard extends StatelessWidget {
@@ -18,42 +20,68 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('ID: $id--Category--$category.\nDescription: $description');
+    int randomColourIndex = 0;
+
+    Random randomChooser = Random();
+    Color selectedColour = Colors.white;
+
+    List<Color> specificColours = [
+      Color(0xFFB2E2ED),
+      Color(0xFFAEC0EC),
+      Color(0xFFABE2D1),
+      Color(0xffA6E8E8),
+      Color(0xff8593FC),
+      Color(0xffF38FA3),
+      Color(0xff6ED1B8),
+      Color(0xff57B4F2),
+      Color(0xffE9A84D),
+      Color(0xff67320B),
+      Color(0xff38497E)
+    ];
+    if (id == null && description == null) {
+      // specific category list
+      randomColourIndex = randomChooser.nextInt(11);
+      selectedColour = specificColours[randomColourIndex];
+    } else {
+      // all categories
+      randomColourIndex = randomChooser.nextInt(16);
+      selectedColour = Colors.accents[randomColourIndex];
+      // selectedColour = Colors.orangeAccent.shade100;
+    }
+
     return GestureDetector(
       onTap: onPress,
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Material(
-          // TODO: Define shape as a vertical rounded rectangle
-          color: Colors.red,
-          elevation: 4,
-          // TODO: Generate random for category & random of an array for
-          //  specific_category color: Colors.orangeAccent.shade100,
-          shadowColor: Colors.black26,
-          borderRadius: BorderRadius.circular(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(id != null ? '$id. $category' : '$category'),
-              Text(
-                description != null ? description! : '',
-                maxLines: 2,
-              ),
-              CircleAvatar(
-                foregroundImage: NetworkImage(
-                  thumbnail!,
-                ),
-                radius: 180,
-                child: Text(
-                  // replace with cache image/gif
-                  '$category',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w900),
+      child: Material(
+        // TODO: Generate random for category & random of an array for specific_category
+        color: selectedColour,
+        elevation: 4,
+        shadowColor: Colors.black26,
+        // TODO: Define shape as a vertical rounded rectangle
+        borderRadius: BorderRadius.circular(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(id != null ? '$id. $category' : '$category'),
+            Text(
+              description != null ? description! : '',
+              maxLines: 2,
+            ),
+            ClipRRect(
+              child: Container(
+                // TODO: Size appropriately
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(thumbnail!),
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
