2dc6a018deddd6d2a031dd1c79f81549314034ee
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-07-14 02:28:38 +0100

    Add more fields in meal_model.dart

--- lib/utilities/meal_model.dart
+++ lib/utilities/meal_model.dart
@@ -17,7 +17,12 @@ class Meal {
       ingredient12,
       ingredient13,
       ingredient14,
-      ingredient15;
+      ingredient15,
+      ingredient16,
+      ingredient17,
+      ingredient18,
+      ingredient19,
+      ingredient20;
 
   // 15 String measures
   final String? measure1,
@@ -34,7 +39,12 @@ class Meal {
       measure12,
       measure13,
       measure14,
-      measure15;
+      measure15,
+      measure16,
+      measure17,
+      measure18,
+      measure19,
+      measure20;
 
   Meal(
       {this.ingredient1,
@@ -52,6 +62,11 @@ class Meal {
       this.ingredient13,
       this.ingredient14,
       this.ingredient15,
+      this.ingredient16,
+      this.ingredient17,
+      this.ingredient18,
+      this.ingredient19,
+      this.ingredient20,
       this.measure1,
       this.measure2,
       this.measure3,
@@ -67,6 +82,11 @@ class Meal {
       this.measure13,
       this.measure14,
       this.measure15,
+      this.measure16,
+      this.measure17,
+      this.measure18,
+      this.measure19,
+      this.measure20,
       required this.id,
       required this.instructions,
       required this.meal,
@@ -99,6 +119,11 @@ class Meal {
       ingredient13: json['strIngredient13'],
       ingredient14: json['strIngredient14'],
       ingredient15: json['strIngredient15'],
+      ingredient16: json['strIngredient16'],
+      ingredient17: json['strIngredient17'],
+      ingredient18: json['strIngredient18'],
+      ingredient19: json['strIngredient19'],
+      ingredient20: json['strIngredient20'],
 
       // Measures
       measure1: json['strMeasure1'],
@@ -116,11 +141,34 @@ class Meal {
       measure13: json['strMeasure13'],
       measure14: json['strMeasure14'],
       measure15: json['strMeasure15'],
+      measure16: json['strMeasure16'],
+      measure17: json['strMeasure17'],
+      measure18: json['strMeasure18'],
+      measure19: json['strMeasure19'],
+      measure20: json['strMeasure20'],
     );
   }
 
   String toString() {
-    return 'idMeal: $id, strMeal: $meal, strMealThumb: $mealThumbnail';
+    return 'idMeal: $id, strMeal: $meal, strMealThumb: $mealThumbnail, '
+        'strInstructions: $instructions, strSource: $source, strYoutube: $ytLink, '
+        'strIngredient1: $ingredient1, strIngredient2: $ingredient2, '
+        'strIngredient3: $ingredient3, strIngredient4: $ingredient4, '
+        'strIngredient5: $ingredient5, strIngredient6: $ingredient6, '
+        'strIngredient7: $ingredient7, strIngredient8: $ingredient8, '
+        'strIngredient9: $ingredient9, strIngredient10: $ingredient10, '
+        'strIngredient11: $ingredient11, strIngredient12: $ingredient12, '
+        'strIngredient13: $ingredient13, strIngredient14: $ingredient14, '
+        'strIngredient15: $ingredient15, strIngredient16: $ingredient16, '
+        'strIngredient17: $ingredient17, strIngredient18: $ingredient18, '
+        'strIngredient19: $ingredient19, strIngredient20: $ingredient20, '
+        'strMeasure1: $measure1, strMeasure2: $measure2, strMeasure3: $measure3, '
+        'strMeasure4: $measure4, strMeasure5: $measure5, strMeasure6: $measure6, '
+        'strMeasure7: $measure7, strMeasure8: $measure8, strMeasure9: $measure9, '
+        'strMeasure10: $measure10, strMeasure11: $measure11, strMeasure12: $measure12, '
+        'strMeasure13: $measure13, strMeasure14: $measure14, strMeasure15: $measure15, '
+        'strMeasure16: $measure16, strMeasure17: $measure17, strMeasure18: $measure18, '
+        'strMeasure19: $measure19, strMeasure20: $measure20';
   }
 }
 
