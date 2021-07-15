8d73593023b4ab20159be2cff1796a2544ea6400
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-07-15 21:44:33 +0100

    Fix: MealPage wasn't getting the right args passed to constructor -- 'category' instead of 'mealId' Style appBar in main.dart Add WelcomePage -- welcome_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:khookbook/pages/category_page.dart';
 import 'package:khookbook/pages/meal_page.dart';
 import 'package:khookbook/pages/specific_category_page.dart';
+import 'package:khookbook/pages/welcome_page.dart';
 import 'package:khookbook/utilities/specific_category_args.dart';
+import 'package:khookbook/utilities/specific_meal_args.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          elevation: 4,
+          centerTitle: true,
+        ),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -26,10 +32,12 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.orange,
       ),
-      initialRoute: CategoryPage.id,
+      initialRoute: WelcomePage.id,
       routes: {
+        /*MealPage.id: (context) => MealPage(mealId: '52874'),*/
+        // TODO: Remove after 'tasting...'
         CategoryPage.id: (context) => CategoryPage(title: 'Recipe Categories'),
-        MealPage.id: (context) => MealPage(meal: ''),
+        WelcomePage.id: (context) => WelcomePage(title: 'Khookbook'),
       },
       onGenerateRoute: (setting) {
         switch (setting.name) {
@@ -40,9 +48,10 @@ class MyApp extends StatelessWidget {
               return SpecificCategoryPage(category: args.category);
             });
           case MealPage.id:
-            final args = setting.arguments as SpecificCategoryArguments;
+            final args = setting.arguments as SpecificMealArguments;
+
             return MaterialPageRoute(builder: (context) {
-              return SpecificCategoryPage(category: args.category);
+              return MealPage(mealId: args.mealId);
             });
           default:
         }
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,74 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:khookbook/components/rounded_button.dart';
+
+class WelcomePage extends StatefulWidget {
+  static const String id = 'welcome';
+  final String title;
+
+  WelcomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: GoogleFonts.rockSalt(
+            color: Color(0xFFFFE5C6),
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xB0FA831D),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              width: 200.0,
+              child: DefaultTextStyle(
+                style: const TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Canterbury',
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    color: Color(0xFFFFE5C6)),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText('Khalifah\'s Cookbook',
+                        cursor: '🍲',
+                        speed: const Duration(milliseconds: 500),
+                        textAlign: TextAlign.center), // "\u{1F372}"
+                    TypewriterAnimatedText('Khookbook!',
+                        cursor: '🍲',
+                        textAlign: TextAlign.center,
+                        speed: const Duration(milliseconds: 250)),
+                  ],
+                  displayFullTextOnTap: true,
+                  totalRepeatCount: 5,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            RoundedButton(
+                colour: Colors.deepOrangeAccent,
+                label: 'Login',
+                onPressed: () {}),
+            RoundedButton(
+                colour: Colors.deepOrange, label: 'Register', onPressed: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
