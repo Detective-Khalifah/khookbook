e11d4141793543026c79a611eaaf57695a107cae
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2025-05-16 00:01:03 +0100

    FIX: clean slate migration to new project.
    feat: Add Windows support for Flutter application
    
    - Created CMakeLists.txt for Windows build configuration.
    - Added Flutter window management with Win32Window class.
    - Implemented resource management including icons and versioning.
    - Integrated Firebase plugins: Cloud Firestore, Firebase Auth, and Firebase Core.
    - Added manifest file with application description and icon configurations.
    - Established console output redirection for debugging.
    - Implemented DPI awareness and dark mode support for the application window.
    - Included necessary utility functions for command line argument handling.

--- android/app/src/main/java/com/blogspot/thengnet/khookbook/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.blogspot.thengnet.khookbook;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/ngnet/khookbook/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ngnet.khookbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
